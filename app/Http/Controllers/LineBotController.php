82a733122f3a3dded35ee5dc7c2ece7576735bf0
中村空海 <nakamurasorami@nakamurakuukainoMacBook-puro.local>
2020-04-21 00:01:03 +0900

    first commit

---
+++ app/Http/Controllers/LineBotController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\Gurunavi;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use LINE\LINEBot;
+use LINE\LINEBot\Event\MessageEvent\TextMessage;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+
+
+class LineBotController extends Controller
+{
+    public function index()
+    {
+        return view('linebot.index');
+    }
+
+    public function restaurants(Request $request)
+    {
+        Log::debug($request->header());
+        Log::debug($request->input());
+
+        $httpClient = new CurlHTTPClient(env('LINE_ACCESS_TOKEN'));
+        $lineBot = new LINEBot($httpClient, ['channelSecret' => env('LINE_CHANNEL_SECRET')]);
+
+        $signature = $request->header('x-line-signature');
+        if (!$lineBot->validateSignature($request->getContent(), $signature)) {
+            abort(400, '署名が無効です');
+        }
+
+        $events = $lineBot->parseEventRequest($request->getContent(), $signature);
+
+        Log::debug($events);
+
+        foreach ($events as $event) {
+            if (!($event instanceof TextMessage)) {
+                Log::debug('Non text message has come');
+                continue;
+            }
+
+            $gurunavi = new Gurunavi();
+            $gurunaviResponse = $gurunavi->searchRestaurants($event->getText());
+
+            if (array_key_exists('error', $gurunaviResponse)) {
+                $replyText = $gurunaviResponse['error'][0]['message'];
+                $replyToken = $event->getReplyToken();
+                $lineBot->replyText($replyToken, $replyText);
+                continue;
+            }
+
+
+            $replyText = '';
+            foreach($gurunaviResponse['rest'] as $restaurant) {
+                $replyText .=
+                    $restaurant['name'] . "\n" .
+                    $restaurant['url'] . "\n" .
+                    "\n";
+            }
+
+            $replyToken = $event->getReplyToken();
+            $replyImg = null;
+            /*{
+                "type": "image",
+                "originalContentUrl": "https://example.com/original.jpg",
+                "previewImageUrl": "https://example.com/preview.jpg"
+            };
+            */
+
+            $lineBot->replyText($replyToken, $replyText, $replyImg);
+        }
+    }
+}
